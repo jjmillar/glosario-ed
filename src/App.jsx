0f038db4d0d9ccd1d6633d8ce5550969b34461cf
Julián Millar <julianjmillar@gmail.com>
2024-01-23 16:05:27 -0300

    mayor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { info } from './assets/info'
+import './App.scss'
 
 function App() {
   return (
-    <>
-      <div>Hola mundo</div>
-    </>
+    <div className='wrapper'>
+      {info.map( (item) => {
+        return (
+          <section key='section' className='section'>
+            <div key='title' className='title'>{item.title}</div>
+            <div key='content' className='content'>{item.content}</div>
+          </section>
+        )
+      })}
+    </div>
   )
 }
 
