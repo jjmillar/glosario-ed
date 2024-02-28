8ff162ffa44d00a18bea676fc63321a80acc270e
Julián Millar <julianjmillar@gmail.com>
2024-02-28 18:20:34 -0300

    added sublist index

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -8,6 +8,8 @@ function Menu () {
                 return(
                     <>
                     {item.title && <li><a href={`#${item.title}`} className='list'>{item.title}</a></li>}
+                    {item.layout && item.layout.map( (i) => {return(<li key={i.name}><a href={`#${i.name}`} className='sub-list'>{i.name}</a></li>)})} 
+                    {item.layout2 && item.layout2.map( (i) => {return(<li key={i.name}><a href={`#${i.name}`} className='sub-list'>{i.name}</a></li>)})}   
                     </>
                 )
             })}
