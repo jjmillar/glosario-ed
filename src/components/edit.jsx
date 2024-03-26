9b3163989c6b8c196088a6588e232cd23f548e16
Julián Millar <julianjmillar@gmail.com>
2024-03-26 09:00:23 -0300

    style and advance

--- src/components/edit.jsx
+++ src/components/edit.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Popup from 'reactjs-popup'
 import './components.scss'
 import { MdOutlineEdit } from 'react-icons/md'
@@ -10,6 +10,13 @@ const Edit = () => {
 
   function delay (ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
   async function close () { await delay(100); setOpen(false) }
+
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault()
+    close()
+    window.alert('Funcion en construccion Q(^.^Q)')
+  }
   return (
     <>
       <IconContext.Provider value={{ size: '2rem' }}>
@@ -19,9 +26,14 @@ const Edit = () => {
       </IconContext.Provider>
       <Popup open={open} closeOnDocumentClick onClose={close}>
         <div className='modal scale-in-br'>
-          <ul className='menu'>
-            <p>kiubo</p>
-          </ul>
+          <form ref={form} onSubmit={sendEmail} className='form'>
+            <label>NOMBRE</label>
+            <input type='text' name='user_name' className='text' />
+            <label>COMENTE DETALLADAMENTE LO QUE SUGUIERE MODIFICAR</label>
+            <textarea name='message' />
+            <input type='submit' value='Enviar' />
+            <a id='link' href='https://wa.me/56972151199?text=kiubo' />
+          </form>
         </div>
       </Popup>
     </>
