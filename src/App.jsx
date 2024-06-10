bb6a8c1d13b3dea4231b912f1135477aa709f92b
Julián Millar <julianjmillar@gmail.com>
2024-06-10 16:28:13 -0400

    fix: fix intro animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,43 +13,45 @@ function App () {
         <Edit />
         <Index />
       </div>
-      {info.map((item) => {
-        return (
-          <section key={item.title} className='section'>
-            {item.title && (<div id={item.title} key='title' className='title'>{item.title}</div>)}
-            {item.subtitle && (<div id={item.subtitle} key='subtitle' className='subtitle'>{item.subtitle}</div>)}
-            {item.content && (<div key='content' className='content'>{item.content}</div>)}
-            {item.img && <img key='image' className='image' src={item.img} />}
-            {item.layout && (
-              <div key='layout-wrapper' id='layout-wrapper' className='layout-wrapper'>
-                {item.layout.map((layout) => {
-                  return (
-                    <div key={layout.name} id={editId(layout.name)} className='layout card'>
-                      {layout.img && (<img src={layout.img} alt={layout.name} className='layout-img' />)}
-                      <div className='layout-content'>
-                        <div className='subtitle'>{layout.name}</div>
-                        <div className='layout-content'>{layout.content}</div>
+      <main className='mainIntroAnimation'>
+        {info.map((item) => {
+          return (
+            <section key={item.title} className='section'>
+              {item.title && (<div id={item.title} key='title' className='title'>{item.title}</div>)}
+              {item.subtitle && (<div id={item.subtitle} key='subtitle' className='subtitle'>{item.subtitle}</div>)}
+              {item.content && (<div key='content' className='content'>{item.content}</div>)}
+              {item.img && <img key='image' className='image' src={item.img} />}
+              {item.layout && (
+                <div key='layout-wrapper' id='layout-wrapper' className='layout-wrapper'>
+                  {item.layout.map((layout) => {
+                    return (
+                      <div key={layout.name} id={editId(layout.name)} className='layout card'>
+                        {layout.img && (<img src={layout.img} alt={layout.name} className='layout-img' />)}
+                        <div className='layout-content'>
+                          <div className='subtitle'>{layout.name}</div>
+                          <div className='layout-content'>{layout.content}</div>
+                        </div>
                       </div>
-                    </div>
-                  )
-                })}
-              </div>
-            )}
-            {item.layout2 && (
-              <div key='layout2-wrapper' id='layout2-wrapper' className='layout2-wrapper'>
-                {item.layout2.map((layout2) => {
-                  return (
-                    <div key={layout2.name} id={layout2.name} className='layout2 card'>
-                      <div className='subtitle'>{layout2.name}:</div>
-                      <div className='layout2-content'>{layout2.content}</div>
-                    </div>
-                  )
-                })}
-              </div>
-            )}
-          </section>
-        )
-      })}
+                    )
+                  })}
+                </div>
+              )}
+              {item.layout2 && (
+                <div key='layout2-wrapper' id='layout2-wrapper' className='layout2-wrapper'>
+                  {item.layout2.map((layout2) => {
+                    return (
+                      <div key={layout2.name} id={layout2.name} className='layout2 card'>
+                        <div className='subtitle'>{layout2.name}:</div>
+                        <div className='layout2-content'>{layout2.content}</div>
+                      </div>
+                    )
+                  })}
+                </div>
+              )}
+            </section>
+          )
+        })}
+      </main>
       <ul key='greetings' id='greetings' className='greetings'>
         {info[info.length - 1].greetings.map((i) => {
           return (
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,23 +1,14 @@
-import { useSpring, animated } from '@react-spring/web'
 import './components.scss'
 
 function IntroAnimation () {
-  const introProps = useSpring({
-    opacity: 0, // Start with opacity 0
-    from: { opacity: 1 }, // Animate from opacity 1
-    delay: 3500
-  })
-
   return (
-    <animated.div style={introProps}>
-      <div className='intro__wrapper'>
-        <div className='intro__text'>PROGRAMA DE ESTUDIO TÉCNICO</div>
-        <div className='intro__text'>ELEMENTAL DOJO Y FILIALES</div>
-        <div className='intro__img--wrapper'>
-          <img src='/LogoED.svg' alt='elemental dojo logo' />
-        </div>
+    <div className='intro__wrapper introAnimation'>
+      <div className='intro__text'>PROGRAMA DE ESTUDIO TÉCNICO</div>
+      <div className='intro__text'>ELEMENTAL DOJO Y FILIALES</div>
+      <div className='intro__img--wrapper'>
+        <img src='/LogoED.svg' alt='elemental dojo logo' />
       </div>
-    </animated.div>
+    </div>
   )
 }
 
